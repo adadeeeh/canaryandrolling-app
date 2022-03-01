8ac69f039f9726b99cecedd67291a4ada079c2da
Faturrahman Ma'ruf <faturrahmanmakruf@gmail.com>
2022-03-01 17:15:23 +0700

    add green instance

--- main.tf
+++ main.tf
@@ -43,7 +43,30 @@ resource "aws_instance" "blue" {
 
 resource "aws_lb_target_group_attachment" "blue" {
   count            = length(aws_instance.blue)
-  target_group_arn = data.terraform_remote_state.network.outputs.target_group_arn
+  target_group_arn = data.terraform_remote_state.network.outputs.tg_blue_arn
   target_id        = aws_instance.blue[count.index].id
+  port             = 80
+}
+
+resource "aws_instance" "green" {
+  count = var.enable_blue_env ? var.blue_instance_count : 0
+
+  ami                    = data.aws_ami.amazon_linux.id
+  instance_type          = "t2.micro"
+  subnet_id              = data.terraform_remote_state.network.outputs.public_subnets[count.index % length(data.terraform_remote_state.network.outputs.public_subnets)]
+  vpc_security_group_ids = [data.terraform_remote_state.network.outputs.web_sg_id]
+  user_data = templatefile("${path.module}/init-script.sh", {
+    file_content = "version 1.1 - #${count.index}"
+  })
+
+  tags = {
+    Name = "green-${count.index}"
+  }
+}
+
+resource "aws_lb_target_group_attachment" "green" {
+  count            = length(aws_instance.green)
+  target_group_arn = data.terraform_remote_state.network.outputs.tg_green_arn
+  target_id        = aws_instance.green[count.index].id
   port             = 80
 }
